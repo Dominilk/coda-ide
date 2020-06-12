c3601e389ecb87cd2ea51dd19d92c27714e19b3d
Dominilk <dominik@diefluchs.at>
2020-06-12 12:57:34 +0200

    Fat jar export now supported

--- src/at/dominik/coda/ide/gui/workspace/editor/SimpleHighlighting.java
+++ src/at/dominik/coda/ide/gui/workspace/editor/SimpleHighlighting.java
@@ -25,19 +25,26 @@ public interface SimpleHighlighting extends EditorHighlighting {
 		}else if(event.getText().equals("[")) {
 			editor.insertText(editor.getCaretPosition(), "]");
 			editor.moveTo(editor.getCaretPosition() - 1);
-		}else if(event.getCode() == KeyCode.ENTER && editor.getCurrentParagraph() >= 1) {
-			editor.insertText(editor.getCaretPosition(), SimpleHighlighting.indent(editor.getText(editor.getCurrentParagraph() - 1)));
+		}else if(event.getText().equals("\"")) {
+			editor.insertText(editor.getCaretPosition(), "\"");
+			editor.moveTo(editor.getCaretPosition() - 1);
 		}
 	}
 	
 	@Override
 	public default void onKeyPress(Editor editor, KeyEvent event) {
-		if(event.getCode() == KeyCode.ENTER && editor.getCaretPosition() >= 2 && editor.getCurrentParagraph() >= 1 && editor.getText(editor.getCaretPosition() - 2, editor.getCaretPosition() - 1).equals("{") && (editor.getParagraphs().size() >= editor.getCurrentParagraph() + 1 || !SimpleHighlighting.indent(editor.getText(editor.getCurrentParagraph())).equals(SimpleHighlighting.indent(editor.getText(editor.getCurrentParagraph()))))) {
+		if(event.getCode() == KeyCode.ENTER && editor.getCaretPosition() >= 2 && editor.getCurrentParagraph() >= 1) {
 			final String indent = SimpleHighlighting.indent(editor.getText(editor.getCurrentParagraph() - 1));
 			
-			editor.insertText(editor.getCaretPosition(), "\n" + indent);
-			editor.insertText(editor.getCurrentParagraph() - 1, 0, "\t");
-			editor.moveTo(editor.getCurrentParagraph(), editor.getText(editor.getCurrentParagraph()).length());
+			final int caretPosition = editor.getCaretPosition();
+			
+			editor.insertText(caretPosition, SimpleHighlighting.indent(editor.getText(editor.getCurrentParagraph() - 1)));
+			
+			if(editor.getText(caretPosition - 2, caretPosition - 1).equals("{") && (editor.getParagraphs().size() >= editor.getCurrentParagraph() + 1 || !SimpleHighlighting.indent(editor.getText(editor.getCurrentParagraph())).equals(SimpleHighlighting.indent(editor.getText(editor.getCurrentParagraph()))))) {
+				editor.insertText(editor.getCaretPosition(), "\n" + indent);
+				editor.insertText(editor.getCurrentParagraph() - 1, 0, "\t");
+				editor.moveTo(editor.getCurrentParagraph(), editor.getText(editor.getCurrentParagraph()).length());
+			}
 			
 		}
 	}
